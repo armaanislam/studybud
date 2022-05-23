660d317539d2fa7d09057d033b82aa2154ff8871
MD. Arman Islam <armaanislam84@gmail.com>
2022-05-23 15:35:43 +0600

    Edit User Account Page

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants'] #When we create a room, host and participation selection won't be shown
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
